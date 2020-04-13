99d01d19f92e1479c60d8894d8210fcf9b90c177
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-04-13 10:01:05 -0300

    Python para Iniciantes

---
+++ Curso Python para Iniciantes_Udemy.py
@@ -0,0 +1,102 @@
+#Curso Python para Iniciantes
+
+#Aula 3 Sobre Python Shell
+'''
+a = 1
+b = 2
+c = 3
+d = a+b+c
+
+print(a)
+print(b)
+print(c)
+print(d)
+'''
+
+#Aula 4 Sobre Conceitos Básicos
+
+#Variáveis
+'''
+valorhora=4
+dias=30
+horastrabalho=8
+vencimentomensal=valorhora*horastrabalho*dias
+nome='romulo'
+
+print(valorhora)
+print(dias)
+print(horastrabalho)
+print(vencimentomensal)
+print(nome)
+'''
+
+'''
+numero1 = 11
+numero2 = 10
+resultado = numero1 + numero2
+print(resultado)
+'''
+
+#Strings
+'''
+nome = "Romulo Vieira"
+print(nome)
+'''
+
+#Listas
+'''
+lista = [1,4,7,"Romulo Vieira",23,14]
+lista.append("python")
+lista.append(20)
+print(lista)
+'''
+
+'''
+lista2 = [1,9,3,4,2,7,6]
+print(lista2)
+
+lista2.sort()
+print(lista2)
+'''
+
+#Tuplas
+'''
+tuplas =("Romulo","python","udemy")
+print(tuplas)
+'''
+
+#For Loop
+'''
+for x in range(0,5):
+    print("Valor de x:",x)
+'''
+
+#While Loop    
+'''
+numero = 15
+while (numero>0):
+    print(numero)
+    numero=numero-1
+'''    
+
+#Pass, Break e Continue
+'''
+numero = 20
+while True:
+    numero = numero - 1
+    print(numero)
+    if (numero==2):
+            break
+'''
+
+'''
+numero = 10
+while True:
+    numero = numero - 1
+    if (numero==4):
+            continue
+    print(numero)
+    if (numero==2):
+        break
+'''
+   
